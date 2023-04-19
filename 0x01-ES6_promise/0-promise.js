a98eeb6ea8e387f2258673b8131103eafecdcff1
Siyabonga Shabalala <dimplezmhengu@gmail.com>
2023-04-19 17:18:59 +0000

    Fix: Return a Promise using this prototype function getResponseFromAPI()

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,15 +1,10 @@
-function getFullResponseFromAPI(success) {
+export default function getResponseFromAPI() {
+  const success = true;
   return new Promise((resolve, reject) => {
     if (success) {
-      const response = {
-        status: 200,
-        body: 'Success'
-      };
-      resolve(response);
+      resolve();
     } else {
-      const error = new Error('The fake API is not working currently');
-      reject(error);
+      reject();
     }
   });
 }
-
