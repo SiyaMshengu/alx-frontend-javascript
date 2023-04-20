b0c06ecbf2226505b34ed951be9df694472128db
Siyabonga Shabalala <dimplezmhengu@gmail.com>
2023-04-20 17:40:56 +0000

    Fix: Implement a class named HolbertonCourse

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,46 +1,53 @@
+/* eslint-disable no-underscore-dangle */
 export default class HolbertonCourse {
-    // Constructor method for creating a new HolbertonCourse object
-    constructor(name, length, students) {
-        this.name = name;
-        this.length = length;
-        this.students = students;
-    }
+  /**
+   * Creates a new HolbertonCourse class
+   *
+   * @param {String} name - The name of the course.
+   * @param {Number} length - How long the course is (in months).
+   * @param {String[]} students - The names of students in the course.
+   *
+   * Implement getters and setters for each attribute
+   */
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
 
-    // Getter method for retrieving the name of the HolbertonCourse object
-    get name() {
-        return this._name;
-    }
-    // Setter method for setting the name of the HolbertonCourse object
-    set name(value) {
-        if (typeof value !== 'string') {
-            throw new TypeError('Name must be a string');
-        }
-        this._name = value;
-    }
+  get name() {
+    return this._name;
+  }
 
-    // Getter method for retrieving the length of the HolbertonCourse object
-    get length() {
-        return this._length;
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
     }
-    // Setter method for setting the length of the HolbertonCourse object
-    set length(value) {
-        if (typeof value !== "number") {
-            throw new TypeError("Length must be a number");
-        }
-        this._length = value;
-    }
+    this._name = value;
+  }
 
-    // Getter method for retrieving the students of the HolbertonCourse object
-    get students() {
-        return this._students;
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
     }
-    // Setter method for setting the students of the HolbertonCourse object
-    set students(value) {
-        value.forEach((element) => {
-            if (typeof element !== 'string') {
-                throw new TypeError('Students must be an array of strings');
-            }
-        });
-        this._students = value;
+    this._length = value;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    if (!(value instanceof Array)) {
+      throw new TypeError('Students must be an array of strings');
     }
+    if (!value.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = value;
+  }
 }
